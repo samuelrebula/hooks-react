71cc4bc9cfba47fa7719c04b8aa0bb32d40e64dd
samuel <rebuuula@gmail.com>
2024-05-15 16:03:22 -0300

    initial commands

--- start.sh
+++ start.sh
@@ -1,7 +1,37 @@
+# Using yarn
+# Create a new React app with TypeScript template
 yarn create react-app hooks-react --template typescript
+
+# Change directory to the newly created app
 cd hooks-react
+
+# Add sass
 yarn add sass
-yarn add react-router-dom   
+
+# Add react-router-dom
+yarn add react-router-dom  
+
+# Add types for react-dom as a development dependency
 yarn add @types/react-dom@latest --dev
 
+# Start the development server
 yarn start
+
+# Using npm
+# Create a new React app with TypeScript template
+npx create-react-app hooks-react --template typescript
+
+# Change directory to the newly created app
+cd hooks-react
+
+# Add sass
+npm install sass
+
+# Add react-router-dom
+npm install react-router-dom
+
+# Add types for react-dom as a development dependency
+npm install @types/react-dom@latest --save-dev
+
+# Start the development server
+npm start
