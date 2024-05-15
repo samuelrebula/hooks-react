dd69330baba402958f4a0fce0b28219e314b2cff
samuel <rebuuula@gmail.com>
2024-05-14 22:05:01 -0300

    New HookUseReducer

--- src/HookUseReducer.tsx
+++ src/HookUseReducer.tsx
@@ -1,22 +1,35 @@
-import { useReducer } from "react";
+import React, { useReducer, useState } from "react";
 
-type State = {
-  counter: number;
-};
+interface Task {
+  name: string;
+  isCompleted: boolean;
+}
 
-type Action = {
-  type: string;
-};
+interface State {
+  tasks: Task[];
+  tasksCount: number;
+}
 
-const reducer = (state: State, action: Action) => {
+type Action =
+  | { type: "add-task"; payload: string }
+  | { type: "toggle-task"; payload: number };
+
+const reducer = (state: State, action: Action): State => {
   switch (action.type) {
-    case "increment":
+    case "add-task":
       return {
-        counter: state.counter + 1,
+        ...state,
+        tasks: [...state.tasks, { name: action.payload, isCompleted: false }],
+        tasksCount: state.tasksCount + 1,
       };
-    case "decrement":
+    case "toggle-task":
       return {
-        counter: state.counter - 1,
+        ...state,
+        tasks: state.tasks.map((item, index) =>
+          index === action.payload
+            ? { ...item, isCompleted: !item.isCompleted }
+            : item
+        ),
       };
     default:
       return state;
@@ -24,15 +37,75 @@ const reducer = (state: State, action: Action) => {
 };
 
 const HookUseReducer: React.FC = () => {
-  const [state, dispatch] = useReducer(reducer, { counter: 0 });
+  const [state, dispatch] = useReducer(reducer, { tasks: [], tasksCount: 0 });
+
+  const [inputValue, setInputValue] = useState("");
 
   return (
     <div>
-      <p>{state.counter}</p>
-      <button onClick={() => dispatch({ type: "increment" })}>Increment</button>
-      <button onClick={() => dispatch({ type: "decrement" })}>Decrement</button>
+      <input
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button
+        onClick={() => {
+          dispatch({ type: "add-task", payload: inputValue });
+          setInputValue("");
+        }}
+      >
+        Adicionar
+      </button>
+
+      {state.tasks.map((task, index) => (
+        <p
+          key={index}
+          onClick={() => dispatch({ type: "toggle-task", payload: index })}
+          style={{ textDecoration: task.isCompleted ? "line-through" : "none" }}
+        >
+          {task.name}
+        </p>
+      ))}
     </div>
   );
 };
 
 export default HookUseReducer;
+
+// import { useReducer } from "react";
+
+// type State = {
+//   counter: number;
+// };
+
+// type Action = {
+//   type: string;
+// };
+
+// const reducer = (state: State, action: Action) => {
+//   switch (action.type) {
+//     case "increment":
+//       return {
+//         counter: state.counter + 1,
+//       };
+//     case "decrement":
+//       return {
+//         counter: state.counter - 1,
+//       };
+//     default:
+//       return state;
+//   }
+// };
+
+// const HookUseReducer: React.FC = () => {
+//   const [state, dispatch] = useReducer(reducer, { counter: 0 });
+
+//   return (
+//     <div>
+//       <p>{state.counter}</p>
+//       <button onClick={() => dispatch({ type: "increment" })}>Increment</button>
+//       <button onClick={() => dispatch({ type: "decrement" })}>Decrement</button>
+//     </div>
+//   );
+// };
+
+// export default HookUseReducer;
